6bc7043f5e8db184a3cf34168f23ee4cddd34cba
haomini <605626708qq@gmail.com>
2017-12-29 11:56:58 +0800

    feat: 决定以原始的方式做下载处理

---
+++ app/src/main/java/com/example/zhiyicx/testdagger2/TestActivity.java
@@ -0,0 +1,13 @@
+package com.example.zhiyicx.testdagger2;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @Describe
+ * @Author zhouhao
+ * @Date 2017/12/29
+ * @Contact 605626708@qq.com
+ */
+
+public class TestActivity extends AppCompatActivity {
+}
--- app/src/test/java/com/example/zhiyicx/testdagger2/DownloadUtilsTest.java
+++ app/src/test/java/com/example/zhiyicx/testdagger2/DownloadUtilsTest.java
@@ -1,13 +1,11 @@
-package com.example.zhiyicx.testdagger2;
+import static org.junit.Assert.*;
 
 /**
  * @Describe
  * @Author zhouhao
- * @Date 2017/12/26
+ * @Date 2017/12/27
  * @Contact 605626708@qq.com
  */
 public class DownloadUtilsTest {
-    public void doSome(){
-        System.out.print("asdasd");
-    }
+
 }
